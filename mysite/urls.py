f94a7ea40bde87782ed17a5e82802e330d35b70a
Valya <lyyy22211@gmail.com>
2020-03-28 20:15:08 +0300

    add path

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from other_app.views import Home
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('', Home.as_view(), name='home'),
+    path('blog/', include('blog.urls'),
     path('', include('blog.urls')),
 ]
